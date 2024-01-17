13d013e01340e3f0746d1ff46ba56bd9f106b575
lumalin022 <lumalin022@student.wethinkcode.co.za>
2024-01-17 19:03:23 +0200

    education page added

---
+++ js/education-script.js
@@ -0,0 +1,25 @@
+
+let yearOne = document.getElementById('year-one-semester-one-btn');
+let yearOneS1 = document.getElementById('year-one-semester-two-btn');
+let year2 = document.getElementById('year-two-btn');
+let rbCollege = document.getElementById('x');
+
+yearOne.addEventListener('click', (event) => {
+     window.open('documents/Lunga_Malinga_Y1S1_Progress_Report.pdf', '_blank', 'fullscreen=yes')
+});
+
+yearOneS1.addEventListener('click', (event) => {
+    window.open('documents/Lunga_Malinga_S2_Progress_Report.pdf', '_blank', 'fullscreen=yes')
+});
+
+year2.addEventListener('click', (event) => {
+    window.open('documents/Lunga_Malinga_S3_Progress_Report.pdf', '_blank', 'fullscreen=yes')
+});
+
+rbCollege.addEventListener('click', (event) => {
+    window.open('documents/14002593 - IIE Marque Export.pdf', '_blank', 'fullscreen=yes')
+});
+
+
+
+     // window.open('documents/Lunga_Malinga_Y1S1_Progress_Report.pdf', '_blank', 'fullscreen=yes')
